88ce75e4418ac8b2adcab7ce9d6a57495f6f857b
Soybean <honghuangdc@gmail.com>
2022-07-01 21:30:23 +0800

    init project

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+// const app = createApp(App)
+
+// app.use(createPinia())
+// app.use(router)
+
+// app.mount('#app')
+
+const app = new Vue({
+  el: '#app',
+  render: (h) => h(App),
+  router
+})
